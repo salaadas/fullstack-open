da664ce3298ed02e033b3a98d947a96c2823f1ea
salaadas <carbonfibersweeper@gmail.com>
2021-12-05 11:16:58 +0700

    remove the now ignored directory

--- part-3/my-node/utils/idGenerator.js
+++
@@ -1,9 +0,0 @@
-const idGenerator = (range = [], listOfIds = []) => {
-  const id = Math.floor(Math.random() * (range[1] - range[0] + 1) + range[0]);
-  if (listOfIds.includes(id)) {
-    idGenerator(range, listOfIds);
-  }
-  return id;
-};
-
-module.exports = idGenerator;
